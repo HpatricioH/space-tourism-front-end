02f592022b2c5622f38a83a68fcb664d22803b8c
HPHuerta <hhernanp@gmail.com>
2022-11-17 23:57:20 -0500

    added tailwind, add navbar and home page

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,6 +1,20 @@
 
 export const HomePage = () => {
   return (
-    <h1>Hello Home Space Page</h1>
+    <section className='flex flex-col justify-center items-center bg-cover h-48 w-64'>
+
+      <div className='text-center'>
+        <p>So, you want to travel to</p>
+        <p>Space</p>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely go to
+          outer space and not hover kind of on the edge of it. Well sit back, and relax
+          because we’ll give you a truly out of this world experience!
+        </p>
+      </div>
+
+      <div><button>Explore</button></div>
+
+    </section>
   )
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+  return (
+    <nav>
+      <ul className='flex flex-col'>
+        <li>00 Home</li>
+        <li>01 Destination</li>
+        <li>02 Crew</li>
+        <li>03 Technology</li>
+      </ul>
+    </nav>
+  )
+}
