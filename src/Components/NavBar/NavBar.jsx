2cd746c6a84218c6baf89e2b1cef7d539db3a9ac
HPHuerta <hhernanp@gmail.com>
2022-11-21 23:58:38 -0500

    added modal style

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import icon from '../../assets/icons/logo.svg'
 import hamburger from '../../assets/icons/icon-hamburger.svg'
 import closeIcon from '../../assets/icons/icon-close.svg'
 import Modal from 'react-modal'
+import './NavBar.css'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
@@ -26,19 +27,44 @@ export const NavBar = () => {
   }
 
   return (
-    <nav className='p-[1rem]'>
+    <nav className='p-[1rem] absolute w-full'>
       <div className='flex flex-row justify-between'>
         <img src={icon} alt='space icon' onClick={handleLink} />
-        <img src={hamburger} alt='hamburger icon' onClick={modalOpen} className='h-[1.35rem] self-center' />
+        <img
+          src={hamburger}
+          alt='hamburger icon'
+          onClick={modalOpen}
+          className='h-[1.35rem] self-center'
+        />
       </div>
-      <Modal isOpen={openModal} closeModal={closeModal}>
-        <img src={closeIcon} alt='close icon' onClick={closeModal} />
-        <ul className='flex flex-col'>
-          <li>00 Home</li>
-          <li>01 Destination</li>
-          <li>02 Crew</li>
-          <li>03 Technology</li>
-        </ul>
+      <Modal
+        isOpen={openModal} closeModal={closeModal} closeTimeoutMS={200} style={{
+          overlay: {
+            position: 'fixed',
+            left: '5.48rem'
+          },
+          content: {
+            position: 'absolute',
+            top: '0px',
+            left: '0px',
+            right: '0px',
+            bottom: '0px',
+            backgroundColor: 'rgba(0, 0, 0, .25)',
+            backdropFilter: 'grayscale(0.5) opacity(0.8) blur(10px)',
+            border: 0,
+            borderRadius: 0
+          }
+        }}
+      >
+        <div className='flex flex-col pt-[1rem]'>
+          <img src={closeIcon} alt='close icon' onClick={closeModal} className='w-[1.1rem] self-end' />
+          <ul className='flex flex-col font-barlow text-[#fff] text-[1.3rem] mt-[3rem] [&_span]:font-bold [&_span]:pr-[2rem] tracking-widest '>
+            <li><span>00</span> Home</li>
+            <li><span>01</span> Destination</li>
+            <li><span>02</span> Crew</li>
+            <li><span>03</span> Technology</li>
+          </ul>
+        </div>
       </Modal>
     </nav>
   )
