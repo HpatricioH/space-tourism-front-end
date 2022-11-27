e142965b7836002bd10f2c58a46f1fc7c8685a3e
HPHuerta <hhernanp@gmail.com>
2022-11-26 23:39:00 -0500

    added redux store and configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { NavBar } from './Components/NavBar/NavBar'
+import { Provider } from 'react-redux'
+import store from './app/store'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <NavBar />
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 )
