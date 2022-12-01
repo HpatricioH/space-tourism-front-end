3f90f18369832b9633cd811c8682793c6e4e8126
HPHuerta <hhernanp@gmail.com>
2022-11-30 22:43:54 -0500

    feat: crewPage functionality

--- src/Components/CrewPage/CrewPage.jsx
+++ src/Components/CrewPage/CrewPage.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getCrewAsync, showCrew } from '../../store/features/crewSlice'
 import { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import crewBg from '../../assets/background crew/background-crew-tablet.jpg'
+import crewBg from '../../assets/background crew/background-crew-mobile.jpg'
 import '../../styles/activeNavBar.css'
 
 export const CrewPage = () => {
@@ -15,8 +15,9 @@ export const CrewPage = () => {
   }, [id])
 
   return (
-    <section className='flex flex-col justify-center items-center gap-20 w-full h-full text-[#fff] font-bellefair'>
-      <div className='w-full h-full'><img src={crewBg} alt='Earth picture' className='object-cover h-fit w-full absolute -z-10' />
+    <section className='flex flex-col justify-center items-center gap-20 w-full h-full text-[#fff] font-bellefair mb-[3rem]'>
+      <div className='w-full h-full'>
+        <img src={crewBg} alt='Earth picture' className='fixed min-h-full min-w-full -z-10' />
         <div className='text-center flex flex-col'>
           <p className='text-[1.1rem] font-barlow tracking-[0.25rem] pt-[5.5rem]'><span className='text-[#49494b] font-extrabold'>02 </span>MEET YOUR CREW</p>
         </div>
@@ -24,12 +25,12 @@ export const CrewPage = () => {
           {crewMembers.map((crew) => {
             return (
               <div key={crew.id} className='px-[1rem]'>
-                <div className='flex flex-col justify-center items-center'>
+                <div className='flex flex-col justify-center items-center gap-4 tracking-wide'>
                   <div className='border-b-[1px] border-[#59595f] w-full flex justify-center items-center'>
                     <img src={crew.images} alt='crew member' className='w-[13rem] pt-[2.5rem]' />
                   </div>
                   <div className='self-center'>
-                    <ul className='relative flex flex-row gap-4 py-6 m-0 p-0 list-none'>
+                    <ul className='relative flex flex-row gap-4 py-2 m-0 p-0 list-none'>
                       <li className='nav__dot-container'><Link to='/crew/1' className={Number(id) === 1 ? 'nav__dot-active' : 'nav__dot'} /></li>
                       <li className='nav__dot-container'><Link to='/crew/2' className={Number(id) === 2 ? 'nav__dot-active' : 'nav__dot'} /></li>
                       <li className='nav__dot-container'><Link to='/crew/3' className={Number(id) === 3 ? 'nav__dot-active' : 'nav__dot'} /></li>
@@ -37,9 +38,9 @@ export const CrewPage = () => {
                     </ul>
                   </div>
                   <div className='text-center'>
-                    <h3 className='uppercase font-bellefair tracking-[2.35px] text-[0.875rem] text-[#838386]'>{crew.role}</h3>
-                    <h2>{crew.name}</h2>
-                    <p>{crew.bio}</p>
+                    <h3 className='uppercase tracking-[1px]  text-[1rem] text-[#838386]'>{crew.role}</h3>
+                    <h2 className='text-[1.5rem] uppercase pt-[0.2rem] pb-[0.8rem] text-[#fff]'>{crew.name}</h2>
+                    <p className='font-barlow tracking-[0.2px] text-[1.08rem] text-[#D0D6F9]'>{crew.bio}</p>
                   </div>
                 </div>
               </div>
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,25 +1,24 @@
-import homeBackground from '../../assets/background-home/background-home-tablet.jpg'
+import homeBackground from '../../assets/background-home/background-home-mobile.jpg'
 
 export const HomePage = () => {
   return (
     <>
 
-      <section className='flex flex-col justify-center items-center gap-20 w-full h-full text-[#fff] font-bellefair'>
+      <section className='flex flex-col justify-center items-center gap-20 w-full h-full text-[#fff] font-bellefair mb-[2rem]'>
         <div className='w-full h-full'><img
-          src={homeBackground} alt='Earth picture' className='object-cover h-fit
-        w-full absolute -z-10'
+          src={homeBackground} alt='Earth picture' className='fixed top-0 left-0 min-h-full min-w-full -z-10'
                                        />
           <div className='text-center flex flex-col px-4'>
-            <p className='text-[1rem] font-barlow tracking-[0.20rem] pt-[8rem]'>SO, YOU WANT TO TRAVEL TO</p>
+            <p className='text-[1rem] font-barlow tracking-[0.20rem] pt-[8rem] text-[#D0D6F9]'>SO, YOU WANT TO TRAVEL TO</p>
             <h1 className='text-[5.3rem] mt-[2rem]'>SPACE</h1>
-            <p className='text-[1.2rem] pt-[2rem] '>
+            <p className='text-[1.2rem] pt-[2rem] text-[#D0D6F9] font-barlow'>
               Let’s face it; if you want to go to space, you might as well genuinely go to
               outer space and not hover kind of on the edge of it. Well sit back, and relax
               because we’ll give you a truly out of this world experience!
             </p>
           </div>
 
-          <div className='mt-[8rem] flex justify-center items-center'>
+          <div className='mt-[6rem] flex justify-center items-center'>
             <button className='bg-white text-[#333] rounded-full w-[10rem] h-[10rem] text-[1.5rem] hover:ring-[2.5rem] hover:outline-none hover:ring-gray-700/[.6]'>EXPLORE</button>
           </div>
         </div>
