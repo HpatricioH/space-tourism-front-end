6607ff4f192b62b6be25b937c8f63ae4d73c56d0
HPHuerta <hhernanp@gmail.com>
2022-12-07 00:44:21 -0500

    feat: tablet style for destination and crew pages

--- src/Components/CrewPage/CrewPage.jsx
+++ src/Components/CrewPage/CrewPage.jsx
@@ -21,16 +21,16 @@ export const CrewPage = () => {
     <section className='flex flex-col justify-center items-center gap-20 w-full h-full text-[#fff] font-bellefair mb-[3rem]'>
       <div className='w-full h-full'>
         <BgImage imageBgMobile={crewBg} imageBgTablet={crewBgTablet} imageBgDesktop={crewBgDesktop} />
-        <div className='text-center flex flex-col'>
+        <div className='text-center flex flex-col min-[640px]:text-left min-[640px]:pl-[1.5rem]'>
           <p className='text-[1.1rem] font-barlow tracking-[0.25rem] pt-[5.5rem]'><span className='text-[#49494b] font-extrabold'>02 </span>MEET YOUR CREW</p>
         </div>
         <div>
           {crewMembers.map((crew) => {
             return (
-              <div key={crew.id} className='px-[1rem]'>
-                <div className='flex flex-col justify-center items-center gap-4 tracking-wide'>
-                  <div className='border-b-[1px] border-[#59595f] w-full flex justify-center items-center'>
-                    <img src={crew.images} alt='crew member' className='w-[13rem] pt-[2.5rem]' />
+              <div key={crew.id} className='px-[1rem] min-[640px]:pt-[2.5rem]'>
+                <div className='flex flex-col justify-center items-center gap-4 tracking-wide min-[640px]:flex-col-reverse min-[640px]:m-auto min-[640px]:w-[70%]'>
+                  <div className='border-b-[1px] border-[#59595f] w-full flex justify-center items-center min-[640px]:border-none'>
+                    <img src={crew.images} alt='crew member' className='w-[13rem] pt-[2.5rem] min-[640px]:w-[25rem]' />
                   </div>
                   <div className='self-center'>
                     <ul className='relative flex flex-row gap-4 py-2 m-0 p-0 list-none'>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom'
+import '../../styles/activeNavBar.css'
 
 export const NavBar = () => {
+  const pathDestinations = window.location.pathname.includes('destinations')
+  const pathCrew = window.location.pathname.includes('crew')
+  const pathTechnology = window.location.pathname.includes('technology')
+
   return (
     <div className='hidden min-[640px]:flex bg-[#171515f7] justify-center items-center'>
       <ul className='uppercase flex flex-row gap-6 px-[2rem] text-white text-sm tracking-[0.18rem] font-barlow'>
-        <li><Link to='/'>home</Link></li>
-        <li><Link to='/destinations/1'>destination</Link></li>
-        <li><Link to='/crew/1'>crew</Link></li>
-        <li><Link to='/technology/1'>technology</Link></li>
+        <li className={window.location.pathname === '/' ? 'navbar__active' : 'navbar__hover'}><Link to='/'>home</Link></li>
+        <li className={pathDestinations ? 'navbar__active' : 'navbar__hover'}><Link to='/destinations/1'>destination</Link></li>
+        <li className={pathCrew ? 'navbar__active' : 'navbar__hover'}><Link to='/crew/1'>crew</Link></li>
+        <li className={pathTechnology ? 'navbar__active' : 'navbar__hover'}><Link to='/technology/1'>technology</Link></li>
       </ul>
     </div>
   )
