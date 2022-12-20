1cfc131c562302dba2388394046ad59e88b1a97d
HPHuerta <hhernanp@gmail.com>
2022-12-19 23:44:37 -0500

    fix: test all objects in payload in redux store for destinations

--- src/tests/components/DestinationSlice.test.jsx
+++ src/tests/components/DestinationSlice.test.jsx
@@ -7,20 +7,19 @@ import { render } from '@testing-library/react'
 
 describe('destinationsSlice', () => {
   it('should handle the GET_DESTINATIONS action', () => {
-    // const destinations = [
-    //   { id: 1, name: 'Moon' },
-    //   { id: 2, name: 'Mars' },
-    //   { id: 3, name: 'Europa' },
-    //   { id: 4, name: 'Titan' }
-    // ]
+    const destinations = [
+      { id: 1, name: 'Moon' },
+      { id: 2, name: 'Mars' },
+      { id: 3, name: 'Europa' },
+      { id: 4, name: 'Titan' }
+    ]
 
     const initialState = { data: [] }
     const action = {
       type: destinationsSlice.actions.getDestinations.type,
-      payload: { id: 1, name: 'Moon' }
+      payload: destinations.map((el) => el)
     }
-    const expectedState = { data: [{ id: 1, name: 'Moon' }] }
-
+    const expectedState = { data: [destinations.map((el) => el)] }
     expect(destinationsSlice.reducer(initialState, action)).toEqual(expectedState)
   })
 
